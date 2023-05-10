f4d03b31e591fab91ded594d83885d942b435e02
josevazf <jose.guilherme.rvf@gmail.com>
2023-05-10 19:25:47 +0100

    deleted

--- testers_zguf/require.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.13;
-
-contract contRequire {
-    function mustNotBeFive(uint256 x) public pure returns (uint256) {
-        require(x != 5, "five is not valid");
-        return x * 2;
-    }
-}
