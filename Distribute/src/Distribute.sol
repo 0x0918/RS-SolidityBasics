960da06968aeaf2ffd3391533c63ddf77673a3bc
jrocha-v <jrocha-v@student.42porto.com>
2023-05-23 16:25:25 +0100

    distribute

--- Distribute/src/Distribute.sol
+++ Distribute/src/Distribute.sol
@@ -8,10 +8,15 @@ contract Distribute {
            array of addresses that is passed as argument.
         2. Write your code in the `distributeEther` function.
     */
-
     constructor() payable {}
 
     function distributeEther(address[] memory addresses) public {
-        // your code here
+        uint256 _balance = address(this).balance;
+		uint256  count = addresses.length;
+		uint256 amount = _balance / count;
+		for (uint256 i = 0; i < addresses.length; i++) {
+			(bool ok,) = addresses[i].call{value: amount}("");
+			require(ok);
+		}
     }
 }
