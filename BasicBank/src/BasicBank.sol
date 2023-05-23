b4cb008dd34bd273090498253176f77f87058c2c
jrocha-v <jrocha-v@student.42porto.com>
2023-05-23 16:42:44 +0100

    basicbank

--- BasicBank/src/BasicBank.sol
+++ BasicBank/src/BasicBank.sol
@@ -6,6 +6,13 @@ contract BasicBank {
     /// @dev it should work properly when called multiple times
     function addEther() external payable {}
 
+	receive() external payable {}
+
     /// @notice used to withdraw ether from the contract (No restriction on withdrawals)
-    function removeEther(uint256 amount) external payable {}
+    function removeEther(uint256 amount) external payable {
+		uint256 _balance = address(this).balance;
+		require(amount <= _balance, "not enough balance");
+		(bool ok, ) = msg.sender.call{value: amount}("");
+		require(ok);
+	}
 }
