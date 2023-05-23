8643d629fe9be16eb9c12a7b10d4b5d3b58906bc
jrocha-v <jrocha-v@student.42porto.com>
2023-05-23 10:46:50 +0100

    priceright

--- PriceIsRight/src/PriceIsRight.sol
+++ PriceIsRight/src/PriceIsRight.sol
@@ -4,6 +4,6 @@ pragma solidity ^0.8.13;
 contract PriceIsRight {
     /// @dev make this function revert unless exactly 1 ether is sent as value in the function call
     function buy() public payable {
-        require(address(this).balance == 1 ether);
-    }
+        require(msg.value == 1 ether, "Exactly 1 ether must be sent");
+	}
 }
